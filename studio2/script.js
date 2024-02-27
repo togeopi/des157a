dcfea60ea648e69a574cd6a4c22830b65c57ac26
togeopi <ckgutierrez@ucdavis.edu>
2024-02-27 13:17:52 -0800

    -[finished studio 2]

--- studio2/script.js
+++ studio2/script.js
@@ -12,7 +12,17 @@
     const p3 = document.getElementById('playlist3');
     const p4 = document.getElementById('playlist4');
     const overlay1 = document.getElementById('overlay1');
+    const overlay2 = document.getElementById('overlay2');
+    const overlay3 = document.getElementById('overlay3');
+    const overlay4 = document.getElementById('overlay4');
     const play1 = document.getElementById('play1');
+    const play2 = document.getElementById('play2');
+    const play3 = document.getElementById('play3');
+    const play4 = document.getElementById('play4');
+    const close1 = document.getElementById('close1');
+    const close2 = document.getElementById('close2');
+    const close3 = document.getElementById('close3');
+    const close4 = document.getElementById('close4');
 
     left.addEventListener('click', function(){
         playlistChange--;
@@ -44,5 +54,33 @@
 
     play1.addEventListener('click', function(){
         overlay1.className = 'overlay';
+    });
+
+    play2.addEventListener('click', function(){
+        overlay2.className = 'overlay';
+    });
+
+    play3.addEventListener('click', function(){
+        overlay3.className = 'overlay';
+    });
+
+    play4.addEventListener('click', function(){
+        overlay4.className = 'overlay';
+    });
+
+    close1.addEventListener('click', function(){
+        overlay1.className = 'hidden';
+    });
+
+    close2.addEventListener('click', function(){
+        overlay2.className = 'hidden';
+    });
+
+    close3.addEventListener('click', function(){
+        overlay3.className = 'hidden';
+    });
+
+    close4.addEventListener('click', function(){
+        overlay4.className = 'hidden';
     });
 })();
